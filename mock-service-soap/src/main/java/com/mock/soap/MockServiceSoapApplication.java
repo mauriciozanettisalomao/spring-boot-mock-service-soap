06b4dee90cd9dc3426fd8952ee4336d16c26c1dd
Zanetti <mauricio.zanetti@dxc.com>
2019-10-07 16:45:10 -0300

    new users

--- mock-service-soap/src/main/java/com/mock/soap/MockServiceSoapApplication.java
+++ mock-service-soap/src/main/java/com/mock/soap/MockServiceSoapApplication.java
@@ -2,9 +2,10 @@ package com.mock.soap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MockServiceSoapApplication {
+public class MockServiceSoapApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MockServiceSoapApplication.class, args);
